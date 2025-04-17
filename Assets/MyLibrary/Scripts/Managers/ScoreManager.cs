0ba5493d7e2e23c1d250aca7d58ec91743ecb805
chloe <chloebean25@gmail.com>
2025-04-17 13:57:16 -0500

    Player Movement
    Added player movement and put textures on the bridge.

--- Assets/MyLibrary/Scripts/Managers/ScoreManager.cs
+++ Assets/MyLibrary/Scripts/Managers/ScoreManager.cs
@@ -1,7 +1,29 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager instance {get; private set;}
+    public float score {get; private set;}
+    public float multiplier {get; private set;}
+    public UnityEvent<ScoreInfo> updateScore;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            //DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            //called on next scene load
+            //instance.score= 0;
+            Destroy(this);
+            return;
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +35,31 @@ public class ScoreManager : MonoBehaviour
     {
         
     }
+
+    public void AddPoints(float _amount, Vector3? _location = null)
+    {
+        score += _amount;
+
+        
+        updateScore.Invoke(new ScoreInfo(score,multiplier, _amount, _location));
+
+    }
 }
+
+
+public class ScoreInfo
+{
+    public float score;
+    public float multiplier;
+    public float delta;
+    public Vector3? location;
+
+    public ScoreInfo(float _score, float _multiplier, float _delta, Vector3? _location = null)
+    {
+        score = _score;
+        multiplier = _multiplier;
+        delta = _delta;
+        location = _location;
+    }
+}
+
